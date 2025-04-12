71d09fe2cd335f2bca67012f2e93fa2216fa0481
tiagoschad <tiagoschad@gmail.com>
2025-04-12 20:15:08 -0300

    componente aulas inserido

--- script.js
+++ script.js
@@ -36,3 +36,89 @@ window.addEventListener('DOMContentLoaded', () => {
       container.classList.remove('dark');
     }
   });  
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const aulas = [
+        {
+            "id": 1,
+            "disciplina": "S05 - Interface Homem-máquina",
+            "data": "ter",
+            "horario": "10:00",
+            "local": "P1-S17",
+            "prova_alert": false,
+            "prova": "12/05",
+            "frequencia": "10/25",
+            "nota": "9"
+        },
+        {
+            "id": 2,
+            "disciplina": "E01 - Circuitos Elétricos em Corrente Contínua",
+            "data": "ter",
+            "horario": "10:00",
+            "local": "P1-S17",
+            "prova_alert": true,
+            "prova": "12/05",
+            "frequencia": "10/25",
+            "nota": "5"
+        },
+        {
+            "id": 3,
+            "disciplina": "M02 - Álgebra e Geometria Analítica",
+            "data": "ter",
+            "horario": "10:00",
+            "local": "P1-S17",
+            "prova_alert": true,
+            "prova": "12/05",
+            "frequencia": "10/25",
+            "nota": "7"
+        }
+    ];
+
+    const aulasDia = aulas.filter(aula => aula.data === "ter"); 
+    const aulasListElement = document.getElementById('aulas-list');
+
+    aulasDia.forEach(aula => {
+        const aulaElement = document.createElement('div');
+        aulaElement.classList.add('comp-aula');
+        aulaElement.setAttribute('data-id', aula.id); 
+
+        let provaDisplay = aula.prova_alert ? '' : 'display: none;';
+        aulaElement.innerHTML = `
+            <div class="lable-prova p_lable" style="${provaDisplay}">PROVA: <b>${aula.prova}</b></div>
+            <div class="titulo_aula">${aula.disciplina}</div>
+            <p class="local-horario">Local e Horário: <b>${aula.local} - ${aula.horario}</b></p>
+            <div class="lables">
+                <div class="lable-frequencia p_lable">FALTAS: <b>${aula.frequencia}</b></div>
+                <div class="lable-nota p_lable">CR: <b>${aula.nota}</b></div>
+            </div>
+        `;
+
+        aulasListElement.appendChild(aulaElement);
+    });
+
+    aulasDia.forEach(aula => 
+        {
+        const nota = Number(aula.nota);
+
+        if (nota < 6) 
+        {
+            const aulaElement = document.querySelector(`[data-id="${aula.id}"]`);
+            const lableNota = aulaElement.querySelector(".lable-nota");
+            lableNota.style.backgroundColor = "red"; 
+        }
+
+        else if(nota >= 8)
+        {
+            const aulaElement = document.querySelector(`[data-id="${aula.id}"]`);
+            const lableNota = aulaElement.querySelector(".lable-nota");
+            lableNota.style.backgroundColor = "green"; 
+        }
+
+        else
+        {
+            const aulaElement = document.querySelector(`[data-id="${aula.id}"]`);
+            const lableNota = aulaElement.querySelector(".lable-nota");
+            lableNota.style.backgroundColor = "orange"; 
+        }
+    });
+});
